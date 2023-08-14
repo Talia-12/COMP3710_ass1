eef14730c45a3c70abde663c09c56bc3d18eca92
Talia-12 <paradox.m.burgess@gmail.com>
2023-08-14 10:45:16 +1000

    Rename testing to exp_and_gabor, change the trig function to be parameterised

--- exp_and_gabor.py
+++ exp_and_gabor.py
@@ -8,6 +8,15 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if (not torch.cuda.is_available()):
 	print("Warning: Cuda not available, using CPU instead.")
 
+# parameters
+amplitude = 5
+frequency = 0.5
+phase     = 0
+direction = np.pi / 6
+
+dirX = np.cos(direction)
+dirY = np.sin(direction)
+
 # grid for computing image, subdivide the space
 X, Y = np.mgrid[-4.0:4:0.01, -4.0:4:0.01]
 
@@ -20,15 +29,16 @@ x = x.to(device)
 y = y.to(device)
 
 # Compute Gaussian
+trigFun = amplitude * torch.sin(frequency * (2*np.pi) * (x * dirX + y * dirY - phase))
 zs = [
   torch.exp(-(x**2+y**2)/2.0),
-  torch.sin(x),
-  torch.exp(-(x**2+y**2)/2.0) * torch.sin(x)
+  trigFun,
+  torch.exp(-(x**2+y**2)/2.0) * trigFun
 ]
 
 #plot
 import matplotlib.pyplot as plt
 for z in zs:
-	plt.imshow(z.cpu().numpy())
+	plt.imshow(z.cpu().numpy(), extent=[-4.0, 4.0, -4.0, 4.0])
 	plt.tight_layout()
 	plt.show()
